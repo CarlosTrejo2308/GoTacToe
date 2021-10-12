e68e323cb0855c6fa9a065789a2e4926097adca5
Carlos N. Trejo <14352052+CarlosTrejo2308@users.noreply.github.com>
2021-10-11 21:43:57 -0500

    feat: add print function to the board

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Player int
 
 const (
@@ -8,8 +10,38 @@ const (
 	human
 )
 
-type Board [3][3]int
+type Board [3][3]Player
+
+func (b Board) String() string {
+	represent := map[Player]string{
+		empty: " ",
+		ia:    "X",
+		human: "O",
+	}
+
+	toPrint := ""
+
+	for i, row := range b {
+		toPrint += fmt.Sprintf("%s | %s | %s", represent[row[0]], represent[row[1]], represent[row[2]])
+		if i < 2 {
+			toPrint += "\n---------\n"
+		}
+	}
+
+	return toPrint
+}
 
 func main() {
-	println("Hello, world.")
+	b := Board{}
+	b[0][0] = ia
+	b[0][1] = human
+	b[0][2] = ia
+	b[1][0] = empty
+	b[1][1] = ia
+	b[1][2] = human
+	b[2][0] = ia
+	b[2][1] = human
+	b[2][2] = ia
+
+	fmt.Println(b)
 }
