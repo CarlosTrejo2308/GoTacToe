ae809e0821f744b05efa270a15506dfde7b1bd6b
Carlos N. Trejo <14352052+CarlosTrejo2308@users.noreply.github.com>
2021-10-11 22:02:18 -0500

    feat: add game struct

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,12 @@ const (
 
 type Board [3][3]Player
 
-func (b Board) String() string {
+type Game struct {
+	board  *Board
+	winner Player
+}
+
+func (g Game) String() string {
 	represent := map[Player]string{
 		empty: " ",
 		ia:    "X",
@@ -21,7 +26,7 @@ func (b Board) String() string {
 
 	toPrint := ""
 
-	for i, row := range b {
+	for i, row := range g.board {
 		toPrint += fmt.Sprintf("%s | %s | %s", represent[row[0]], represent[row[1]], represent[row[2]])
 		if i < 2 {
 			toPrint += "\n---------\n"
@@ -31,17 +36,72 @@ func (b Board) String() string {
 	return toPrint
 }
 
-func main() {
-	b := Board{}
-	b[0][0] = ia
-	b[0][1] = human
-	b[0][2] = ia
-	b[1][0] = empty
-	b[1][1] = ia
-	b[1][2] = human
-	b[2][0] = ia
-	b[2][1] = human
-	b[2][2] = ia
+func NewGame() *Game {
+	return &Game{
+		board:  &Board{},
+		winner: empty,
+	}
+}
 
-	fmt.Println(b)
+func (b *Board) isFull() bool {
+	for _, row := range b {
+		for _, cell := range row {
+			if cell == empty {
+				return false
+			}
+		}
+	}
+
+	return true
+}
+
+func (b *Board) isWinning(player Player) bool {
+	// Check rows
+	for _, row := range b {
+		if row[0] == player && row[1] == player && row[2] == player {
+			return true
+		}
+	}
+
+	// Check columns
+	for i := 0; i < 3; i++ {
+		if b[0][i] == player && b[1][i] == player && b[2][i] == player {
+			return true
+		}
+	}
+
+	// Check diagonals
+	if b[0][0] == player && b[1][1] == player && b[2][2] == player {
+		return true
+	}
+	if b[0][2] == player && b[1][1] == player && b[2][0] == player {
+		return true
+	}
+
+	return false
+}
+
+func (b *Board) play(player Player, x, y int) error {
+	if b[x][y] != empty {
+		return fmt.Errorf("Cell (%d, %d) is already taken", x, y)
+	}
+
+	b[x][y] = player
+	return nil
+}
+
+func (g *Game) StillPlaying() bool {
+	b := g.board
+	return !b.isFull() && g.winner == empty
+}
+
+func main() {
+	g := NewGame()
+
+	/* for b.StillPlaying() {
+		b.HumanTurn()
+		b.IATurn()
+	} */
+
+	fmt.Println(g)
 }
